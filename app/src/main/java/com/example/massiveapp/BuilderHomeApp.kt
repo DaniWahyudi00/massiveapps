6feaad69c2b1ba8113c132769b90cf5ea25b401d
Dwi Azi Prasetya <dwiaziprasetya456@gmail.com>
2024-05-18 00:14:42 +0700

    Create component BottomNavigation and add NavHost also Navigation class

--- app/src/main/java/com/example/massiveapp/BuilderHomeApp.kt
+++ app/src/main/java/com/example/massiveapp/BuilderHomeApp.kt
@@ -1,9 +1,42 @@
 package com.example.massiveapp
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.massiveapp.navigation.Screen
+import com.example.massiveapp.screens.component.BottomNavigation
+import com.example.massiveapp.screens.home.HomeScreen
+import com.example.massiveapp.ui.theme.MassiveappTheme
 
 @Composable
-fun BuilderHomeApp(modifier: Modifier = Modifier) {
-    
+fun BuilderHomeApp(
+    navController: NavHostController = rememberNavController(),
+) {
+    Scaffold(
+        bottomBar = { BottomNavigation(navController = navController) }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = Screen.Home.route) {
+                HomeScreen()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun BuilderHomeAppPreview() {
+    MassiveappTheme {
+        BuilderHomeApp()
+    }
 }
---
+++ app/src/main/java/com/example/massiveapp/screens/component/BottomNavigation.kt
@@ -0,0 +1,104 @@
+package com.example.massiveapp.screens.component
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.graphics.toColorInt
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.massiveapp.R
+import com.example.massiveapp.navigation.NavigationItem
+import com.example.massiveapp.navigation.Screen
+import com.example.massiveapp.ui.theme.MassiveappTheme
+
+@Composable
+fun BottomNavigation(
+    navController: NavHostController
+) {
+    NavigationBar(
+        containerColor = Color.White
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        val navigationItems = listOf(
+            NavigationItem(
+                title = "Home",
+                icon = painterResource(R.drawable.icon_home_outlined),
+                iconActive = painterResource(R.drawable.icon_home_filled),
+                screen = Screen.Home
+            ),
+            NavigationItem(
+                title = "Service",
+                icon = painterResource(R.drawable.icon_elementplus_outlined),
+                iconActive = painterResource(R.drawable.icon_elementplus_filled),
+                screen = Screen.Service
+            ),
+            NavigationItem(
+                title = "Transaction",
+                icon = painterResource(R.drawable.icon_transactionminus_outlined),
+                iconActive = painterResource(R.drawable.icon_transactionminus_filled),
+                screen = Screen.Transaction
+            ),
+            NavigationItem(
+                title = "Profile",
+                icon = painterResource(R.drawable.icon_user_outlined),
+                iconActive = painterResource(R.drawable.icon_user_filled),
+                screen = Screen.Profile
+            ),
+        )
+        navigationItems.map { item ->
+            val isSelected = currentRoute == item.screen.route
+            NavigationBarItem(
+                selected = isSelected,
+                onClick = {},
+                icon = {
+                    if (isSelected) {
+                        Icon(
+                            painter = item.iconActive,
+                            contentDescription = item.title,
+                            tint = Color("#5E8451".toColorInt())
+                        )
+                    } else {
+                        Icon(
+                            painter = item.icon,
+                            contentDescription = item.title,
+                        )
+                    }
+                },
+                label = {
+                    if (isSelected) {
+                        Text(
+                            text = item.title,
+                            color = Color("#5E8451".toColorInt())
+                        )
+                    } else {
+                        Text(
+                            text = item.title,
+                        )
+                    }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    indicatorColor = Color.White
+                )
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun BottomNavigationPreview() {
+    MassiveappTheme {
+        BottomNavigation(navController = rememberNavController())
+    }
+}
